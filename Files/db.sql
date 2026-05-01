95f0a2bd2cb6b3a386c86aad99ee6e453bf33801
Yasiru De Silva <ygimres2@gmail.com>
2026-05-01 19:41:41 +0530

    Add files via upload

---
+++ Files/db.sql
@@ -0,0 +1,264 @@
+CREATE DATABASE  IF NOT EXISTS `shortstay` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `shortstay`;
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: shortstay
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking` (
+  `booking_id` int NOT NULL AUTO_INCREMENT,
+  `guest_id` int DEFAULT NULL,
+  `property_id` int DEFAULT NULL,
+  `checkin_date` date DEFAULT NULL,
+  `checkout_date` date DEFAULT NULL,
+  `total_price` decimal(10,2) DEFAULT NULL,
+  `status` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`booking_id`),
+  KEY `guest_id` (`guest_id`),
+  KEY `property_id` (`property_id`),
+  CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`guest_id`) REFERENCES `guest` (`user_id`),
+  CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `complain`
+--
+
+DROP TABLE IF EXISTS `complain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `complain` (
+  `complaint_id` int NOT NULL AUTO_INCREMENT,
+  `booking_id` int DEFAULT NULL,
+  `description` text,
+  `status` varchar(30) DEFAULT NULL,
+  `priority` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`complaint_id`),
+  KEY `booking_id` (`booking_id`),
+  CONSTRAINT `complain_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`booking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `guest`
+--
+
+DROP TABLE IF EXISTS `guest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `guest` (
+  `user_id` int NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `guest_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `customer` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `host`
+--
+
+DROP TABLE IF EXISTS `host`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `host` (
+  `user_id` int NOT NULL,
+  `bank_details` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `host_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `customer` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `inspection`
+--
+
+DROP TABLE IF EXISTS `inspection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inspection` (
+  `inspection_id` int NOT NULL AUTO_INCREMENT,
+  `property_id` int DEFAULT NULL,
+  `inspector_id` int DEFAULT NULL,
+  `scheduled_date` date DEFAULT NULL,
+  `overall_score` decimal(3,2) DEFAULT NULL,
+  `recommendation` text,
+  `status` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`inspection_id`),
+  KEY `property_id` (`property_id`),
+  KEY `inspector_id` (`inspector_id`),
+  CONSTRAINT `inspection_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`),
+  CONSTRAINT `inspection_ibfk_2` FOREIGN KEY (`inspector_id`) REFERENCES `staff` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment` (
+  `payment_id` int NOT NULL AUTO_INCREMENT,
+  `booking_id` int DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `payment_date` date DEFAULT NULL,
+  PRIMARY KEY (`payment_id`),
+  KEY `booking_id` (`booking_id`),
+  CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`booking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `property`
+--
+
+DROP TABLE IF EXISTS `property`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `property` (
+  `property_id` int NOT NULL AUTO_INCREMENT,
+  `host_id` int DEFAULT NULL,
+  `title` varchar(150) DEFAULT NULL,
+  `description` text,
+  `address` varchar(255) DEFAULT NULL,
+  `price_per_night` decimal(10,2) DEFAULT NULL,
+  `max_guests` int DEFAULT NULL,
+  `verification_badge` tinyint(1) DEFAULT '0',
+  `is_approved` tinyint(1) DEFAULT '0',
+  `available_dates` text,
+  `overall_score` decimal(3,2) DEFAULT NULL,
+  `recommendations` text,
+  `image` varchar(255) DEFAULT NULL,
+  `verification_requested` tinyint(1) DEFAULT '0',
+  `verification_status` enum('none','requested','inspecting','approved','rejected') DEFAULT 'none',
+  PRIMARY KEY (`property_id`),
+  KEY `host_id` (`host_id`),
+  CONSTRAINT `property_ibfk_1` FOREIGN KEY (`host_id`) REFERENCES `host` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `property_availability`
+--
+
+DROP TABLE IF EXISTS `property_availability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `property_availability` (
+  `availability_id` int NOT NULL AUTO_INCREMENT,
+  `property_id` int NOT NULL,
+  `available_date` date NOT NULL,
+  `is_booked` tinyint DEFAULT '0',
+  PRIMARY KEY (`availability_id`),
+  KEY `property_id` (`property_id`),
+  CONSTRAINT `property_availability_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review` (
+  `review_id` int NOT NULL AUTO_INCREMENT,
+  `booking_id` int DEFAULT NULL,
+  `property_id` int DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  `comment` text,
+  `review_date` date DEFAULT NULL,
+  `host_response` text,
+  `response_date` date DEFAULT NULL,
+  PRIMARY KEY (`review_id`),
+  KEY `booking_id` (`booking_id`),
+  KEY `property_id` (`property_id`),
+  CONSTRAINT `review_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`booking_id`),
+  CONSTRAINT `review_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`),
+  CONSTRAINT `review_chk_1` CHECK ((`rating` between 1 and 5))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff` (
+  `user_id` int NOT NULL,
+  `hire_date` date DEFAULT NULL,
+  `role` enum('admin','payment_manager','field_inspector') DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('guest','host','admin','payment_manager','field_inspector') DEFAULT NULL,
+  `membership_level` enum('basic','silver','gold') DEFAULT 'basic',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-28 10:30:34
